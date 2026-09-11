2ef4a388dc5df89729bd4baef92d6d50ed27c9c2
ampandey <amritpandey2005@gmail.com>
2026-09-12 00:58:37 +0530

    import addresses related logics from order service

--- alembic/env.py
+++ alembic/env.py
@@ -8,10 +8,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from app.core.config import settings
 from app.db.database import Base
-from app.models.products import Product
-from app.models.user import User
+from app.models.address import Address
 from app.models.cart import Cart
 from app.models.cart_items import CartItem
+from app.models.products import Product
+from app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -38,6 +39,14 @@ target_metadata = Base.metadata
 # ... etc.
 
 
+def include_object(object, name, type_, reflected, compare_to):
+    ignored_tables = ["orders", "order_items", "order_service_alembic_version"]
+    
+    if type_ == "table" and name in ignored_tables:
+        return False
+    return True
+
+
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -56,6 +65,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        include_object=include_object
     )
 
     with context.begin_transaction():
@@ -79,6 +89,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
+            include_object=include_object
         )
 
         with context.begin_transaction():
--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends
 
 from app.api.deps import require_admin
-from app.api.v1.endpoints import admin, auth, cart, products, upload, user
+from app.api.v1.endpoints import address, admin, auth, cart, products, upload, user
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 api_router.include_router(user.router, prefix="/user", tags=["User"])
+api_router.include_router(address.router, prefix="/address", tags=["Addresses"])
 api_router.include_router(
     admin.router, prefix="/admin", tags=["Admin"], dependencies=[Depends(require_admin)]
 )
